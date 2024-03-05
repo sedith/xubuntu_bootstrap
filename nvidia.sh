0a0c9d50d66b0c9dc8407af69c19ae42ba59e540
sedith <martin.jacquet@posteo.net>
2024-03-05 11:48:50 +0100

    comment style change

--- nvidia.sh
+++ nvidia.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# drivers
+## drivers
 echo "check recommended nvidia driver version:"
 ubuntu-drivers devices
 
-sudo apt-get install nvidia-driver-525
+sudo apt-get install nvidia-driver-525  # TODO: find version of interest
 
-# prime selector
+## prime selector
 sudo apt-get install nvidia-prime
 sudo prime-select nvidia
 
-# gpu monitoring
+## gpu monitoring
 sudo add-apt-repository ppa:flexiondotorg/nvtop
 sudo apt install nvtop
--- robotpkg.sh
+++ robotpkg.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-# cf. http://robotpkg.openrobots.org/install.html
+## cf. http://robotpkg.openrobots.org/install.html
 
-# Prerequisites
+## prerequisites
 sudo apt-get install -y bzip2 libbz2-dev pax tar tcl-dev tk-dev flex bison libtool-bin doxygen libudev-dev libsdl2-dev asciidoctor
 
-# Clone
+## clone
 memory = `pwd`
 cd ~/work/
 git clone git://git.openrobots.org/robots/robotpkg
 
-# Boostrap openrobots
+## boostrap openrobots
 cd robotpkg/bootstrap
 bootstrap --prefix=$ROBOTPKG_BASE
 
-# Copy config
+## copy config
 cp $memory/config_files/openrobots/robotpkg.conf $ROBOTPKG_BASE/etc/
 
-# Install set
+## install set
 cd ~/work/robotpkg/
 make update-myset
